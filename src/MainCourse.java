3f25fc77d5b373a67f563025a68444f671334cd7
nathidentified <nathanielmortel15@gmail.com>
2025-12-05 22:05:29 +0800

    Update MainCourse.java

--- src/MainCourse.java
+++ src/MainCourse.java
@@ -6,25 +6,58 @@ class MainCourse extends MenuItem {
 
     @Override
     public void selectVariation(Scanner sc) {
-        System.out.println("\nSelect type:");
-        if (name.equals("Pasta")) {
-            System.out.println("1. Spaghetti\n2. Carbonara");
-            System.out.print("Enter your choice: ");
-            int choice = sc.nextInt();
-            sc.nextLine();
-            variation = (choice == 1) ? "Spaghetti" : "Carbonara";
-        } else if (name.equals("Burger")) {
-            System.out.println("1. Plain");
-            System.out.print("Enter your choice: ");
-            int choice = sc.nextInt();
-            sc.nextLine();
-            variation = "Plain";
-        } else if (name.equals("Fries")) {
-            System.out.println("1. Cheese\n2. Sour Cream");
-            System.out.print("Enter your choice: ");
-            int choice = sc.nextInt();
-            sc.nextLine();
-            variation = (choice == 1) ? "Cheese" : "Sour Cream";
+
+        boolean loop = true;
+
+        while (loop) {
+
+            System.out.println("\nSelect type:");
+
+            if (name.equals("Pasta")) {
+                System.out.println("1. Spaghetti\n2. Carbonara");
+                System.out.print("Enter your choice: ");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                if(choice == 1){
+                    variation = "Spaghetti";
+                    loop = false;
+                } else if (choice == 2) {
+                    variation = "Carbonara";
+                    loop = false;
+                }
+                else {
+                    System.out.println("Invalid choice. Choose only Spaghetti or Carbonara.");
+                }
+            } else if (name.equals("Burger")) {
+                System.out.println("1. Plain");
+                System.out.print("Enter your choice: ");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                if (choice == 1){
+                    variation = "Plain";
+                    loop = false;
+                }else {
+                    System.out.println("Invalid choice. Choose only Spaghetti or Carbonara.");
+                }
+            } else if (name.equals("Fries")) {
+                System.out.println("1. Cheese\n2. Sour Cream");
+                System.out.print("Enter your choice: ");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                if (choice == 1){
+                    variation = "Cheese";
+                    loop = false;
+                } else if (choice == 2) {
+                    variation = "Sour Cream";
+                    loop = false;
+                }
+                else {
+                    System.out.println("Invalid choice. Choose only Cheese and Sour Cream.");
+                }
+            }
+
         }
+
     }
 }
+
